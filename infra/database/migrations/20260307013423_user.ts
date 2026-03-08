68177b2e75d9af17de29a316b812d71a04f0babc
Joao Santos <dev.joaosantos@outlook.com>
2026-03-08 17:29:54 -0400

    add timestamps for user table

--- infra/database/migrations/20260307013423_user.ts
+++ infra/database/migrations/20260307013423_user.ts
@@ -2,8 +2,10 @@ import type { Knex } from "knex";
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable("user", (table) => {
-    table.uuid("id");
+    table.uuid("id").primary();
     table.string("name", 255).notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
   });
 }
 
