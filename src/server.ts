28d22d55082c0771b89b6b9c8d569433ea24fca3
Joao Santos <dev.joaosantos@outlook.com>
2026-03-12 11:57:36 -0400

    run migrations before start server

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
+import { db } from "../infra/database/database.ts";
+import { env } from "../infra/environment.ts";
 import { app } from "./app.ts";
 
-app.listen({ port: 8000 }, (error, address) => {
+app.listen({ host: env.HOST, port: env.PORT }, async (error, address) => {
   if (error) {
     console.error("error:", error);
     process.exit(1);
   }
   console.log(`server running at ${address}`);
+
+  try {
+    const migrateStatus = await db.migrate.status();
+    if (migrateStatus < 0) {
+      db.migrate.latest();
+    }
+  } catch (error) {
+    db.migrate.latest();
+  }
 });
